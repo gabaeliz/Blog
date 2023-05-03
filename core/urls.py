3104e7560e02bef61904349dde1ad7a64419590a
gabaeliz <elizabethg.eg12@gmail.com>
2023-05-02 22:30:54 -0600

    Configurated urlpatterns

--- core/urls.py
+++ core/urls.py
@@ -15,8 +15,13 @@ Including another URLconf
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
 from django.contrib import admin
-from django.urls import path
+from django.views.generic import RedirectView
+from django.conf import settings
+from django.conf.urls.static import static
+from django.urls import path, include
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-]
+    path('blog/', include('blog.urls')),
+    path('', RedirectView.as_view(url = '/blog/', permanent=True)),
+] + static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
